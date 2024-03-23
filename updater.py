3b47e1466b7fea1809668d94dff68dc0fc8580da
dsglazyrin <dsglazyrin@gmail.com>
2024-03-24 01:53:02 +0500

    miners

--- updater.py
+++ updater.py
@@ -6,6 +6,7 @@ def self_update():
     logging.info('Self updating...')
     os.system('cd /miner/clore_rent_server_agent')
     os.system('git pull https://github.com/dsglazyrin/clore_rent_server_agent.git')
+    os.system('supervisorctl reload')
     exit()
 
 
